da25cd120500dac01558db4cb433a4cd5a833526
2883857@student.edu.az <2883857@student.edu.az>
2023-10-03 16:30:06 +0400

    tryna to add audio

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// let x = document.getElementById("myAudio").autoplay;
+
+// function playAudio() {
+//     x.play();
+// }
+
 alert("Kəmərləri bağlıyın, getdik!");
 
 let q1 = prompt("2x2");
@@ -18,11 +24,12 @@ if (q1 == 4) {
                 alert("Doğrudur");
                 alert("Əla, onda biraz da çətin");
 
-                // prompt()
-                let q5 = prompt("Verilmiş cümlədəki hansı 'da(də)' bitişik və ya aralı yazılmalıdır.","Kursa gələndə hansı yolda çətinliklər yarana bilər yada düşməz yada ev tapşırığını yerinə yetirmək.");
+            
+                let q5 = prompt("Verilmiş cümlədəki hansı 'da(də)' bitişik və ya aralı yazılmalıdır.", "Kursa gələndə hansı yolda çətinliklər yarana bilər yada düşməz yada ev tapşırığını yerinə yetirmək.");
                 if (q5 == "Kursa gələndə hansı yolda çətinliklər yarana bilər ya da düşməz yada ev tapşırığını yerinə yetirmək.") {
-                    alert("Bravo, təbriklər!");
-                    
+                                        alert("Bravo, təbriklər!");
+
+                    // playAudio();
 
                 } else {
                     alert("Yanlışdır");
@@ -43,3 +50,5 @@ if (q1 == 4) {
 } else {
     alert("Yanlışdır")
 }
+
+
